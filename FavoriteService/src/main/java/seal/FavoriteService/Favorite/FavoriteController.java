71c49c1dbc6a41ac0c7826df1b5dd8ab0b799523
TanawatRyu <gunmag3307985@hotmail.com>
2018-11-07 15:03:15 +0700

    [EDIT] master : edit controller

---
+++ FavoriteService/src/main/java/seal/FavoriteService/Favorite/FavoriteController.java
@@ -0,0 +1,34 @@
+package seal.FavoriteService.Favorite;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FavoriteController {
+
+    @Autowired
+    private FavoriteService favoriteService;
+
+    @GetMapping("/user/{user_id:[\\s]}/favorites")
+    public ResponseEntity<List<Favorite>> getAllFavorite(@PathVariable(name = "user_id") String userId) {
+        List<Favorite> favorites_object = favoriteService.getFavoriteByUserId(userId);
+        return new ResponseEntity<List<Favorite>>(favorites_object, HttpStatus.OK);
+    }
+
+    @PostMapping("/user/{user_id:[\\s]}/subject/{subject_id:[\\d]}/favorite")
+    public ResponseEntity<Favorite> createFavorite(@PathVariable(name = "user_id") String userId,
+                                                   @PathVariable(name = "subject_id") int subjectId) {
+        Favorite favorite_object = favoriteService.createFavorite(userId, subjectId);
+        return new ResponseEntity<Favorite>(favorite_object, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/favorite/{favorite_id:[\\d]}")
+    public ResponseEntity<Favorite> deleteFavorite(@PathVariable(name = "favorite_id") int id) {
+        Favorite favorite_object = favoriteService.deleteFavoriteById(id);
+        return new ResponseEntity<Favorite>(favorite_object, HttpStatus.OK);
+    }
+}
